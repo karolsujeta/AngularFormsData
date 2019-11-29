e6a7aaebd1aa21e8ea3db9f23db32e21726b015e
unknown <karol.sujeta@outlook.com>
2019-11-29 03:12:07 +0100

    app-forms-data

--- src/app/todo-form/todo-form.component.ts
+++ src/app/todo-form/todo-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { debounceTime } from 'rxjs/internal/operators';
 
 @Component({
   selector: 'app-todo-form',
@@ -15,6 +16,7 @@ export class TodoFormComponent implements OnInit {
   newemail: String = '';
   newsex: String = '';
 
+  
   @Output()
   event = new EventEmitter<any>();
   constructor(private formBuilder: FormBuilder) { }
@@ -27,6 +29,43 @@ export class TodoFormComponent implements OnInit {
       email: ['', [Validators.required, Validators.pattern('[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}')]],
       sex: ['', Validators.required]
     });
+
+    this.modelForm.valueChanges.pipe(debounceTime(1000)).subscribe((value) => {
+      this.onControlValueChanged();
+    });
+
+    this.onControlValueChanged(); // ustawiamy początkowany stan walidacji
+
+  }
+
+  formErrors = {
+    firstname: '',
+    lastname: '',
+    zip: '',
+    email: '',
+    sex: '',
+  }
+
+  private validationMessages = {
+    firstname: {
+      required: 'Nie wprowadzono imienia!',
+      minlength: 'Wprowadzono za mało znaków! (minimum 2)',
+      maxlength: 'Wprowadzono zbyt dużo znaków! (maximum 15)'
+    },
+    lastname: {
+      required: 'Nie wprowadzono nazwiska!',
+      minlength: 'Wprowadzono zbyt mało znaków! (minimum 2)',
+      maxlength: 'Wprowadzono zbyt dużo znaków! (maximum 25)'
+    },
+    zip: {
+      required: 'Nie wprowadzono kodu pocztowego!',
+      pattern: 'Wprowadzono zły format!'
+
+    },
+    email: {
+      required: 'Nie wprowadzono adresu email!',
+      pattern: 'Wprowadzono zły format!'
+    }
   }
 
   onSubmit(form) {
@@ -42,5 +81,21 @@ export class TodoFormComponent implements OnInit {
       sex: this.newsex
     }
     this.event.emit(newElement);
+  }
+
+  onControlValueChanged() {
+    const form = this.modelForm;
+
+    for (let field in this.formErrors) {
+      this.formErrors[field] = '';
+      let control = form.get(field);
+
+      if (control && control.dirty && !control.valid) {
+        const validationMessages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += validationMessages[key] + ' ';
+        }
+      }
+    }
   }
 }
